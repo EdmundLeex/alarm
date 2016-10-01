1eea48be52ddf27c7eadfe9e872b075ecc70a062
Edmund Li <edmund.xz.lee@gmail.com>
2016-10-01 16:40:48 -0700

    added fb login

---
+++ alarm/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  alarm
+//
+//  Created by edmund on 10/1/16.
+//  Copyright © 2016 edmund. All rights reserved.
+//
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
